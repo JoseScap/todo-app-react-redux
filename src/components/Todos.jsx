d8a8eb565cdf70ca8bc87bd2fc828b3fff565d02
Juan Jose, Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2021-02-25 02:58:12 -0300

    switchig todos on differents tabs

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,14 +3,25 @@ import Todo from './Todo';
 
 const Todos = () => {
   const todos = useSelector(state => state.todos.todos);
+  const tab = useSelector(state => state.tabs.tab);
 
   return (
     <section className="todos">
       <div className="container">
         {
-          todos.map((todo, i) => (
+          todos.map((todo, i) => ( tab === 1 && (
             <Todo todo={todo} key={i}/>
-          ))
+          )))
+        }
+        {
+          todos.map((todo, i) => (tab === 2 && !todo.done && (
+            <Todo todo={todo} key={i}/>
+          )))
+        }
+        {
+          todos.map((todo, i) => (tab === 3 && todo.done && (
+            <Todo todo={todo} key={i}/>
+          )))
         }
       </div>
     </section>
